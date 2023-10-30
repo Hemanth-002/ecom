a2fc7e04de328ce1980b4c79aa0aedfa338fa3fb
Hemanth-002 <hemanth.sairam@fullness.io>
2023-10-30 21:01:06 +0530

    Feat : Create Hero Section for Shopee

---
+++ client/components/ButtonPrimary.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+
+const Button = styled.button`
+  width: fit-content;
+  height: fit-content;
+  background-color: #555;
+  border-color: #333;
+  border-width: 0px;
+  margin: 0;
+  padding: 0.75rem 1.625rem 0.75rem 1.625rem;
+  border-radius: 2.875rem;
+  color: #eee;
+  letter-spacing: 1px;
+  line-height: 1.5rem;
+`;
+
+const ButtonPrimary = ({ text }) => {
+  return <Button>{text}</Button>;
+};
+
+export default ButtonPrimary;
